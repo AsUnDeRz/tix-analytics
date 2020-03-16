812ff5bcebb986c0fddac00c5403821f22fbca45
ToCHe <rerom2536@gmail.com>
2020-03-16 15:55:36 +0700

    add firebase analytics

--- lib/src/core.dart
+++ lib/src/core.dart
@@ -4,6 +4,7 @@
  * found in the LICENSE file.
  */
 import 'package:device_info/device_info.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:sentry/sentry.dart';
 import 'package:tix_analytics/src/device_info.dart';
@@ -16,22 +17,28 @@ class TixAnalytics {
 
   final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
   SentryClient sentry;
+  FirebaseAnalytics firebaseAnalytics;
   Map<String, dynamic> tagsDeviceInfo = {};
 
   TixAnalytics._init();
 
-  Future init({String dsn = ''}) async {
+  Future init({String dsn = '', FirebaseAnalytics analytics}) async {
     if (dsn.isNotEmpty) {
       sentry = new SentryClient(dsn: dsn);
+    }
+    if (analytics != null) {
+      firebaseAnalytics = analytics;
     }
     tagsDeviceInfo = await mapperDeviceInfo(deviceInfoPlugin);
     return Future.value({debugPrint(tagsDeviceInfo.toString())});
   }
 
-  void tix(dynamic) {
+  void tix(dynamic) async {
+    assert(dynamic != null);
     switch (dynamic.runtimeType) {
       case TixEvent:
         log(dynamic.name, dynamic.values);
+        await logFireBaseAnalytics(dynamic);
         break;
       case TixError:
         logError(dynamic.name, dynamic.error, dynamic.stackTrace);
@@ -47,10 +54,24 @@ class TixAnalytics {
   }
 
   void logError(String name, dynamic error, dynamic stackTrace) {
-    if (sentry != null) {
-      sentry.capture(
-          event: Event(
-              loggerName: name, exception: error, stackTrace: stackTrace, tags: tagsDeviceInfo));
+    assert(() {
+      if (sentry != null) {
+        sentry.capture(
+            event: Event(
+                loggerName: name, exception: error, stackTrace: stackTrace, tags: tagsDeviceInfo));
+      }
+      return true;
+    }());
+  }
+
+  Future<void> logFireBaseAnalytics(TixEvent event) async {
+    if (firebaseAnalytics != null) {
+      return await firebaseAnalytics.logEvent(
+        name: event.name,
+        parameters: event.values,
+      );
+    } else {
+      return Future.value();
     }
   }
 }
